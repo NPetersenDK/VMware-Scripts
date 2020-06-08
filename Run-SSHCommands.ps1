1d8ec778f38741b9b53b3d9663b0aa2551951136
Nikolaj Petersen <n@nipetersen.dk>
2020-06-08 19:48:25 +0200

    Create Run-SSHCommands.ps1

---
+++ Run-SSHCommands.ps1
@@ -0,0 +1,39 @@
+<#
+.Author
+    Nikolaj Petersen
+    n@nipetersen.dk
+
+.Description
+  This script was created due to an error in NSX where the cluster will go into 100% CPU mode.
+  The fix for that was to disable the agent by hand on all hosts, which takes some time manually.
+  This script might be usable for other things aswell, therefore this script is generic.
+
+What it does:
+    1) Firstly we connect to the VCSA asking for credentials
+    2) Asking for the SSH Creds
+    3) Getting the hosts in the cluster of variable $esxhosts
+    4) SSH'ing into them and executing command of "Invoke-SSHCommand"
+       
+#>
+
+## Parameters
+$vcsa = "VCSA.domain.tld"
+$Cluster = "ClusterName"
+
+Connect-VIServer $vcsa -Credential (Get-Credential -Message "Credentials for VCSA")
+
+$sshcred = (Get-Credential -Message "SSH Credentials for ESX hosts in cluster")
+$esxhosts = Get-Cluster $Cluster | Get-VMHost 
+
+foreach ($esxhost in $esxhosts) {
+    Write-Host $esxhost.Name
+
+    Start-VMHostService -HostService ($esxhost | Get-VMHostService | Where { $_.Key -eq "TSM-SSH"})
+    $sshsession = New-SSHSession -ComputerName $esxhost -Credential $sshcred
+    Invoke-SSHCommand -Index $sshsession.Sessionid -Command "/etc/init.d/vShield-Stateful-Firewall stop"
+    Remove-SSHSession -Index $sshsession.Sessionid
+
+}
+
+## Nulling SSH Creds
+$sshcred = $null
